9e2a88f74fedaea00cea913c4ccd079cfd07895b
Scott Greenlay <sgreenlay@gmail.com>
2019-08-12 21:28:21 -0700

    Update packages and start playing with a database

--- server/index.js
+++ server/index.js
@@ -1,2 +1,48 @@
 
-console.log("Hello World!");
+const sqlite3 = require('sqlite3').verbose();
+
+function dateToSQLDate(date) {
+    var pad = function (num) {
+        return ('00' + num).slice(-2)
+    };
+    return date.getUTCFullYear() + '-' +
+        pad(date.getUTCMonth() + 1) + '-' +
+        pad(date.getUTCDate());
+}
+
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function () {
+
+    db.run(`
+        CREATE TABLE stocks (
+            ticker TEXT,
+            UNIQUE(ticker))
+    `);
+
+    var stmt = db.prepare("INSERT OR IGNORE INTO stocks VALUES (?)");
+    stmt.run("MSFT");
+    stmt.finalize();
+
+    db.run(`
+        CREATE TABLE stock_prices (
+            ticker TEXT,
+            date TEXT,
+            open NUMBER,
+            close NUMBER,
+            high NUMBER,
+            low NUMBER,
+            UNIQUE(ticker, date))
+    `);
+
+    var stmt = db.prepare("INSERT OR IGNORE INTO stock_prices VALUES (?, ?, ?, ?, ?, ?)");
+    stmt.run("MSFT", dateToSQLDate(new Date()), 0, 0, 0, 0);
+    stmt.run("MSFT", dateToSQLDate(new Date()), 0, 0, 0, 0);
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, ticker, date FROM stock_prices", function (err, row) {
+        console.log(row.id + ": " + row.ticker + " " + row.date);
+    });
+});
+
+db.close();
